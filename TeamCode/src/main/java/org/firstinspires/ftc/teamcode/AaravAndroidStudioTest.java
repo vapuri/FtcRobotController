e1844823422bed2b97fe58aa6b08b39a96e4ab4d
CoderApuri <puri.aarav@gmail.com>
2023-09-01 17:38:56 -0700

    First Commit To Test Android Studio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AaravAndroidStudioTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AaravAndroidStudioTest.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name="Aarav: Turn Test with Power Cap", group="Linear Opmode")
+@TeleOp(name="AaravAndroidStudioTest", group="Linear Opmode")
 public class AaravAndroidStudioTest extends LinearOpMode {
 
     private DcMotor motorfrontleft;
