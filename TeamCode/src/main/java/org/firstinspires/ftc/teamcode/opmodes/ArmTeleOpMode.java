fa9f43e21eafe8dde28b47083bd983a487d3cf41
CoderApuri <puri.aarav@gmail.com>
2023-10-13 15:22:07 -0700

    Added ArmTeleOpMode that uses Arm class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmTeleOpMode.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.Arm;
+
+@TeleOp
+public class ArmTeleOpMode extends OpMode {
+    Arm arm;
+
+    @Override
+    public void init() {
+        arm = new Arm(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.b)  // Check if the 'B' button is pressed
+            arm.rotateUp();
+        else if (gamepad1.y)  // Check if the 'Y' button is pressed
+            arm.rotateDown();
+        telemetry.addData("Target Position", arm.getCurrentPosition());
+        telemetry.update();
+    }
+}
