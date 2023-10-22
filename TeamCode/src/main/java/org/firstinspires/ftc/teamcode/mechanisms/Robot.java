0907d530e2aee0d518e2c6c046ebc2094d8f388a
Varun Puri <varun@varun.org>
2023-10-22 13:12:54 -0700

    Removed color distance sensor code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Robot.java
@@ -1,23 +1,19 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
-
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Robot {
     DriveTrain drivetrain;
-    ColorDistanceSensor colorDistanceSensor;
     String name = "FTC New Era";
 
     public Robot(HardwareMap hardwareMap) {
         drivetrain = new DriveTrain(hardwareMap);
-        // colorDistanceSensor = new ColorDistanceSensor(hardwareMap);
+
     }
 
     public DriveTrain getDrivetrain() {
         return drivetrain;
     }
-    public ColorDistanceSensor getColorDistanceSensor() {
-        return colorDistanceSensor;
-    }
+
     public String getName() {
         return name;
     }
